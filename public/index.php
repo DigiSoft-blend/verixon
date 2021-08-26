5e323fbc2eb4ae87f488c548f2c7cfabec972263
DigiSoft-blend <u.silas@yahoo.com>
2021-08-26 23:16:00 +0100

    optimised

--- public/index.php
+++ public/index.php
@@ -8,8 +8,8 @@
  */ 
 
 
- require_once __DIR__.'/../autoload.php'; //require verixons autoloader
-//require('../vendor/autoload.php');
+ //require_once __DIR__.'/../autoload.php'; //require verixons autoloader
+ require('../vendor/autoload.php');
  $configcon = include __DIR__.'/../config/configcon.php'; //require verixons default configuration
 
  use App\bootstrap\App;
