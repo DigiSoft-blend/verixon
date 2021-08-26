54f78c3ba4d6c269b74750615e11b977d63808d2
DigiSoft-blend <u.silas@yahoo.com>
2021-08-27 00:12:04 +0100

    optimised

--- routes.php
+++ routes.php
@@ -15,20 +15,20 @@ use App\container\ContainerResolver;
  * @author   Silas Udofia <Silas@Verixon.com>
  */
 
-Route::get('/', function(){ ContainerResolver::get('Verixon' ,'index');});
-Route::get('/gettingStarted', function(){ ContainerResolver::get('Verixon', 'gettingStarted');});
-Route::get('/not-connected', function(){ ContainerResolver::get('Verixon', 'noNetworkConnection');});  
-Route::get('/select/name/cost/id', function(){  ContainerResolver::get('Product', 'selectProduct');} , 'name/cost/id');
-Route::get('/delete/id', function(){ ContainerResolver::get('Product', 'deleteProduct');}, 'id');
-Route::get('/registerUser', function(){ ContainerResolver::get('UserAuthController', 'registerUser');});
-Route::get('/loginUser', function(){ ContainerResolver::get('UserAuthController', 'login');});
-Route::get('/logoutUser', function(){ ContainerResolver::get('UserAuthController', 'logout');});
-Route::get('/deleteUser', function(){ ContainerResolver::get('UserAuthController', 'deleteUser');});
-Route::get('/sendEmail', function(){ ContainerResolver::get('EmailController', 'Email');});
-Route::get('/upload', function(){ ContainerResolver::get('UploadController', 'uploadFile');});
-Route::get('/deleteFile', function(){ ContainerResolver::get('UploadController', 'deleteFile');});
-Route::get('/Test', function(){ ContainerResolver::get('TestClass','printMessage', true)->printMessage();});
-Route::get('/encrypt', function(){ ContainerResolver::get('CryptoController', 'AsymetriceEncryptMessage');});
-Route::get('/decrypt', function(){ ContainerResolver::get('CryptoController', 'AsymetriceDecryptMessage');});
+// Route::get('/', function(){ ContainerResolver::get('Verixon' ,'index');});
+// Route::get('/gettingStarted', function(){ ContainerResolver::get('Verixon', 'gettingStarted');});
+// Route::get('/not-connected', function(){ ContainerResolver::get('Verixon', 'noNetworkConnection');});  
+// Route::get('/select/name/cost/id', function(){  ContainerResolver::get('Product', 'selectProduct');} , 'name/cost/id');
+// Route::get('/delete/id', function(){ ContainerResolver::get('Product', 'deleteProduct');}, 'id');
+// Route::get('/registerUser', function(){ ContainerResolver::get('UserAuthController', 'registerUser');});
+// Route::get('/loginUser', function(){ ContainerResolver::get('UserAuthController', 'login');});
+// Route::get('/logoutUser', function(){ ContainerResolver::get('UserAuthController', 'logout');});
+// Route::get('/deleteUser', function(){ ContainerResolver::get('UserAuthController', 'deleteUser');});
+// Route::get('/sendEmail', function(){ ContainerResolver::get('EmailController', 'Email');});
+// Route::get('/upload', function(){ ContainerResolver::get('UploadController', 'uploadFile');});
+// Route::get('/deleteFile', function(){ ContainerResolver::get('UploadController', 'deleteFile');});
+// Route::get('/Test', function(){ ContainerResolver::get('TestClass','printMessage', true)->printMessage();});
+// Route::get('/encrypt', function(){ ContainerResolver::get('CryptoController', 'AsymetriceEncryptMessage');});
+// Route::get('/decrypt', function(){ ContainerResolver::get('CryptoController', 'AsymetriceDecryptMessage');});
 
 
