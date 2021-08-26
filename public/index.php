27e596c78c5cb5e5d143486fa6e0d8a96c747f93
DigiSoft-blend <u.silas@yahoo.com>
2021-08-26 23:00:44 +0100

    optimised

--- public/index.php
+++ public/index.php
@@ -9,13 +9,13 @@
 
 
 //  require_once __DIR__.'/../vendor/autoload.php'; //require verixons autoloader
+require('../vendor/autoload.php');
+ $configcon = include __DIR__.'/../config/configcon.php'; //require verixons default configuration
 
-//  $configcon = include __DIR__.'/../config/configcon.php'; //require verixons default configuration
-
-//  use App\bootstrap\app;
+ use App\bootstrap\app;
  
-//   $verixon = new App(); //creates a new verixon app instance
+  $verixon = new App(); //creates a new verixon app instance
  
 
 
-echo "verixon";
+//echo "verixon";
